57b77b065bdcfe1140b3b16f086b8e81e122c9d4
Armando Bustos <armanjbustos596@gmail.com>
2016-12-19 09:43:29 -0600

    Initial commit

---
+++ js/activity.js
@@ -0,0 +1,18 @@
+define(function (require) {
+    var activity = require("sugar-web/activity/activity");
+    var datastore = require("sugar-web/datastore");
+
+
+    // Manipulate the DOM only when it is ready.
+    require(['domReady!'], function (doc) {
+
+
+        // Initialize the activity.
+        activity.setup();
+
+        // HERE GO YOUR CODE
+
+
+    });
+
+});
